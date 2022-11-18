6bfb39a690e6c94319a696e27fe5bef9130e48ca
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-11-18 17:04:27 -0300

    finish

--- src/routes/guess.ts
+++ src/routes/guess.ts
@@ -1,9 +1,64 @@
 import fastify, { FastifyInstance } from "fastify";
+import { number, string, z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get("/guesses/count", async () => {
     const count = await prisma.guess.count();
     return { count };
   });
+
+  fastify.post(
+    "/pools/:poolId/games/:gameId/guesses",
+    {
+      onRequest: [authenticate],
+    },
+    async (request, reply) => {
+      const createGuessParams = z.object({
+        poolId: z.string(),
+        gameId: z.string(),
+      });
+
+      const createGuessBody = z.object({
+        firstTeamPoints: z.number(),
+        secondTeamPoints: z.number(),
+      });
+
+      const { poolId, gameId } = createGuessParams.parse(request.params);
+      const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(
+        request.body
+      );
+      const participant = await prisma.participant.findUnique({
+        where: {
+          userId_poolId: {
+            poolId,
+            userId: request.user.sub,
+          },
+        },
+      });
+
+      if (!participant) {
+        return reply.status(400).send({
+          message: "You're not allowed to create a guess inside this pool",
+        });
+      }
+
+      const guess = await prisma.guess.findUnique({
+        where: {
+          participantId_gameId: {
+            participantId: participant.id,
+            gameId,
+          },
+        },
+      });
+
+      return {
+        poolId,
+        gameId,
+        firstTeamPoints,
+        secondTeamPoints,
+      };
+    }
+  );
 }
